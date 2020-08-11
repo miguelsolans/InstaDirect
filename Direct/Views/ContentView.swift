176f731fdbff42903586d59f8f6eb4958dcee937
Miguel Solans <dev.miguelsolans@gmail.com>
2020-08-12 00:41:36 +0100

    Fixed Navbar Titles

--- Direct/Views/ContentView.swift
+++ Direct/Views/ContentView.swift
@@ -10,23 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var networkManager = NetworkManager()
+    @State private var username: String = ""
+    @State private var password: String = ""
     
     var body: some View {
+        
         NavigationView {
             
             if networkManager.loading {
                 Text("Loading...")
-            } else if networkManager.error {
-                Text("Error!")
-            } else {
+            } else if !networkManager.error && networkManager.direct.status != "fail" {
+                
                 List((networkManager.direct.inbox?.threads)!, id: \.thread_id) { thread in
                     
                     NavigationLink(destination: ThreadView(thread: thread)) {
                         ThreadRow(thread: thread)
                     }
-                }
+                }.navigationBarTitle(Text("Direct"))
+                    .navigationBarItems(leading:
+                        Button(action: {
+                            
+                            self.networkManager.loadMore(cursor: self.networkManager.direct.inbox!.oldest_cursor)
+                            print("Load more...")
+                        }) {
+                            Text("More")
+                        }
+                    )
+            
+            
+            } else {
+                Text("Error!")
             }
-        }.navigationBarTitle("Direct")
+        }
     }
 }
 
