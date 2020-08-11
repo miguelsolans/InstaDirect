25f4094ccf08a69048ad9a382f49b862b1ca81eb
Miguel Solans <dev.miguelsolans@gmail.com>
2020-08-12 00:44:11 +0100

    Create MediaView.swift

---
+++ Direct/Views/MediaView.swift
@@ -0,0 +1,21 @@
+//
+//  MediaView.swift
+//  Direct
+//
+//  Created by Miguel Solans on 11/08/2020.
+//  Copyright © 2020 Miguel Solans. All rights reserved.
+//
+
+import SwiftUI
+
+struct MediaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MediaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaView()
+    }
+}
